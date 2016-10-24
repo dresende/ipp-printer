33c7eda55f04c60d2aede8e5fedbf23bcd0e1811
Diogo Resende <dresende@thinkdigital.pt>
2016-10-24 10:26:46 +0100

    spaces:2 -> tabs, adds commas

--- bin.js
+++ bin.js
@@ -1,25 +1,32 @@
 #!/usr/bin/env node
-'use strict'
+"use strict";
 
-var config = require('rc')('ipp-server', {
-  name: 'ipp-server', dir: process.cwd(), port: 3000
-})
-var nonPrivate = require('non-private-ip')
-var url = require('url')
-var ip = nonPrivate() || nonPrivate.private()
-var fs = require('fs')
+var config = require("rc")("ipp-server", {
+	name : "ipp-server",
+	dir  : process.cwd(),
+	port : 3000
+});
+var nonPrivate = require("non-private-ip");
+var url        = require("url");
+var fs         = require("fs");
+var ip         = nonPrivate() || nonPrivate.private();
 
-var Printer = require('./')
+var Printer = require("./");
 
-var p = new Printer(config)
+var p = new Printer(config);
 
-p.on('job', function (job) {
-  var filename = 'printjob-' + job.id + '-' + Date.now() + '.ps'
-  job.pipe(fs.createWriteStream(filename)).on('finish', function () {
-    console.log('printed:', filename)
-  })
-})
+p.on("job", function (job) {
+	var filename = "printjob-" + job.id + "-" + Date.now() + ".ps";
 
-p.server.on('listening', function () {
-  console.log('ipp-server listening on:', url.format({protocol: 'http', hostname: ip, port: config.port}))
-})
+	job.pipe(fs.createWriteStream(filename)).on("finish", function () {
+		console.log("printed:", filename);
+	});
+});
+
+p.server.on("listening", function () {
+	console.log("ipp-server listening on:", url.format({
+		protocol : "http",
+		hostname : ip,
+		port     : config.port
+	}));
+});
